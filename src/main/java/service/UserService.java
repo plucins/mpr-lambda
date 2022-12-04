a933bf021912ed4eef3128203f10176adf33f88e
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2022-12-04 20:24:45 +0100

    lambda exercises

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,50 @@
+package service;
+
+import model.Role;
+import model.User;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class UserService {
+
+    public static List<User> findUsersWhoHaveMoreThanOneAddress(List<User> users) {
+        //todo
+        return null;
+    }
+
+    public static User findOldestPerson(List<User> users) {
+        //todo
+        return null;
+    }
+
+    public static User findUserWithLongestUsername(List<User> users) {
+        //todo
+        return null;
+    }
+
+    public static String getNamesAndSurnamesCommaSeparatedOfAllUsersAbove18(List<User> users) {
+        //todo
+        return null;
+    }
+
+    public static List<String> getSortedPermissionsOfUsersWithNameStartingWithA(List<User> users) {
+        //todo
+        return null;
+    }
+
+    public static void printCapitalizedPermissionNamesOfUsersWithSurnameStartingWithS(List<User> users) {
+        //todo
+    }
+
+    public static Map<Role, List<User>> groupUsersByRole(List<User> users) {
+        //todo
+        return null;
+    }
+
+    public static Map<Boolean, List<User>> partitionUserByUnderAndOver18(List<User> users) {
+        //todo
+        return null;
+    }
+}
